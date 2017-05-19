fe5dd13102647796404f8fdc291e8c5ea57ff4fd
tangfangwei0 <jacktang2008@163.com>
2017-05-19 16:28:11 +0800

    test

---
+++ src/main/java/demo/msa/framework/registry/ServiceRegistry.java
@@ -0,0 +1,10 @@
+package demo.msa.framework.registry;
+
+/**
+ * Created by tangfw on 2017/5/14.
+ */
+public interface ServiceRegistry {
+
+    void register(String serviceName, String serviceAddress);
+
+}
